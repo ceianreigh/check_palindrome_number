d3f76d1c84eeb4d891c6e1e50984971465f3469b
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 00:46:32 +0800

    Write the instruction and pseudocode

---
+++ check_palindrome_number.py
@@ -0,0 +1,13 @@
+# Write a program to check if the given number is a palindrome number.
+
+# pseudocode
+
+# ask user to enter a number
+
+# reverse the given number
+
+# check if the given number is equal to the reversed number
+
+# if yes, return true
+
+# else return false
