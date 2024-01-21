2498f03b029b7ec73563c56e1ae04d3b24dfd474
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 01:00:33 +0800

    Write the codes that reverse the given number

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -3,12 +3,17 @@
 # pseudocode
 
 # ask user to enter a number
-number = int(input("Enter a number:"))
+number = int(input("Enter a number: "))
 
 # write a message on what the user entered
 print("The original number is", number)
 
 # reverse the given number
+rev_number = 0
+while number > 0:
+    remainder = number % 10
+    rev_number = (rev_number * 10) + remainder
+    number = number // 10
 
 # check if the given number is equal to the reversed number
 
